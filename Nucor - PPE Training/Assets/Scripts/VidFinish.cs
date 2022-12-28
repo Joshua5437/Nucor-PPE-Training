6b88c4aa7868b8338e1233ee4bce06c0ae690c9e
Joshua Cotton <jic77@msstate.edu>
2022-12-28 17:57:56 -0600

    Added Videos
    - Added Background Video
    - Added Audio to Gloves Usage Video

--- Nucor - PPE Training/Assets/Scripts/VidFinish.cs
+++ Nucor - PPE Training/Assets/Scripts/VidFinish.cs
@@ -3,15 +3,16 @@ using UnityEngine.UI;
 
 public class VidFinish : MonoBehaviour
 {
+    private bool FirstPlayFlag = true;
     public GameObject CurrentVideoImageGameobject;
     public UnityEngine.Video.VideoPlayer CurrentVideo, NextVideo;
 
     void Update()
     {
-        if (!CurrentVideo.isPlaying && (CurrentVideoImage.activeSelf == false))
+        if (!CurrentVideo.isPlaying && FirstPlayFlag && (CurrentVideoImageGameobject.activeSelf == false))
         {
             NextVideo.Play();
-            CurrentVideoImage.SetActive(false);
+            CurrentVideoImageGameobject.SetActive(false);
         }
         else {
         }
